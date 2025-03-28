02ee8e9c17429bf323b2ed98d4d45fbb487d47aa
fahim2k25 <fahim.program@gmail.com>
2025-03-29 03:54:34 +0600

    working on dynamic buttons from api

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function loadCatagories() {
+    // 1. Fetch the data
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+        // 2. Convert Promise to JSON
         .then((res) => res.json())
-        .then((data) => displayCatagories(data));
+        // 3. Send data to display
+        .then((data) => displayCatagories(data.categories));
 
 }
 
-function displayCatagories(elem) {
+function displayCatagories(arrr) {
+    // 1.get the contianer
+    const cataCont = document.getElementById("category-container");
 
-    console.log(elem);
+    //2.Loop operation on an ARRAY of object
+    arrr.forEach(element => {
+
+        //Create an Element
+        const newBtn = document.createElement("button");
+        newBtn.innerText = element.category;
+        newBtn.classList.add("btn", "btn-sm");
+
+        //Append that element
+        cataCont.appendChild(newBtn);
+    });
+
+
+
 }
 
 loadCatagories();
